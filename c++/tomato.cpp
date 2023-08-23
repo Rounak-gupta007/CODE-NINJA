cfb90ec3bde5c270671db810032776595fb598f6
Rounak-gupta007 <142198182+Rounak-gupta007@users.noreply.github.com>
2023-08-23 23:09:51 +0530

    23-08-2023 (LAST CODE OF THE DAY)
    code for tomato

---
+++ c++/tomato.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main()
+{
+    using namespace std;
+    int tomatos;
+    tomatos=25;
+    cout<<"I have";
+    cout<<tomatos;
+    cout<<"tomatos";
+    cout<<endl;
+    tomatos = tomatos-1;
+    cout<<"Now I have"<<tomatos<<"tomatos."<<endl;
+    return 0;
+
+}
