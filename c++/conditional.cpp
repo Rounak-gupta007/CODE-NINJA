7bc8b3d6aef8541cf3f4adab3ba2f2bb46acbff1
Rounak-gupta007 <142198182+Rounak-gupta007@users.noreply.github.com>
2023-09-08 22:57:00 +0530

    08-09-2023
    DID MANY CODES TODAY YAY!!

---
+++ c++/conditional.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter a and b: ";
+    cin>>a>>b;
+    (a>b)?cout<<"a is greater.\n":cout<<"b is greater.\n";
+
+    return 0;
+}
---
+++ c++/relationaloperator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=5,b=6,c,d,e,f,g,h;
+    c=a>b;
+    d=a<b;
+    e=(4!=3);
+    f=(8>=17);
+    g=(123<=123);
+    h=(a==b);
+    cout<<"Value of c = "<< c <<endl;
+    cout<<"Value of d = "<< d <<endl;
+    cout<<"Value of e = "<< e <<endl;
+    cout<<"Value of f = "<< f <<endl;
+    cout<<"Value of g = "<< g <<endl;
+    cout<<"Value of h = "<< h <<endl;
+    return 0;
+
+}
